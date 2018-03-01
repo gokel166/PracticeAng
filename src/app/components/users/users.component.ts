486a317cdb16e797165176a4b7fb4b29442de466
gokel166 <okellogeorge40@yahoo.com>
2018-03-01 01:24:55 -0600

    Modify array of users with events

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,60 +8,137 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  showExtended = true;
+  loaded = false;
 
   constructor() { }
 
   ngOnInit() {
-    this.users = [
-      {
-        firstName: 'George',
-        middleInit: 'W',
-        lastName: 'Okello',
-        age: 27,
-        address: {
-          street: '2020 McCausland Ave',
-          city: 'St. Louis',
-          state: 'MO',
-          zip: 63143
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'George',
+          middleInit: 'W',
+          lastName: 'Okello',
+          age: 27,
+          address: {
+            street: '2020 McCausland Ave',
+            city: 'St. Louis',
+            state: 'MO',
+            zip: 63143
+          }
+        },
+        {
+          firstName: 'Arthur',
+          middleInit: 'T',
+          lastName: 'Okello',
+          age: 24,
+          address: {
+            street: '1326 McKelvey Road',
+            city: 'Maryland Heights',
+            state: 'MO',
+            zip: 63043
+          }
+        },
+        {
+          firstName: 'Nigel',
+          middleInit: 'C',
+          lastName: 'Okello',
+          age: 19,
+          address: {
+            street: '1326 McKelvey Road',
+            city: 'Maryland Heights',
+            state: 'MO',
+            zip: 63043
+          }
+        },
+        {
+          firstName: 'Chris',
+          middleInit: 'A',
+          lastName: 'Okello',
+          age: 24,
+          address: {
+            street: '1326 McKelvey Road',
+            city: 'Maryland Heights',
+            state: 'MO',
+            zip: 63043
+          }
         }
-      },
-      {
-        firstName: 'Arthur',
-        middleInit: 'T',
-        lastName: 'Okello',
-        age: 24,
-        address: {
-          street: '1326 McKelvey Road',
-          city: 'Maryland Heights',
-          state: 'MO',
-          zip: 63043
-        }
-      },
-      {
-        firstName: 'Nigel',
-        middleInit: 'C',
-        lastName: 'Okello',
-        age: 19,
-        address: {
-          street: '1326 McKelvey Road',
-          city: 'Maryland Heights',
-          state: 'MO',
-          zip: 63043
-        }
-      },
-      {
-        firstName: 'Chris',
-        middleInit: 'A',
-        lastName: 'Okello',
-        age: 24,
-        address: {
-          street: '1326 McKelvey Road',
-          city: 'Maryland Heights',
-          state: 'MO',
-          zip: 63043
-        }
-      }
-    ];
+      ];
+
+      this.loaded = true;
+    }, 2000);
+
+
+    // this.users = [
+    //   {
+    //     firstName: 'George',
+    //     middleInit: 'W',
+    //     lastName: 'Okello',
+    //     age: 27,
+    //     address: {
+    //       street: '2020 McCausland Ave',
+    //       city: 'St. Louis',
+    //       state: 'MO',
+    //       zip: 63143
+    //     }
+    //   },
+    //   {
+    //     firstName: 'Arthur',
+    //     middleInit: 'T',
+    //     lastName: 'Okello',
+    //     age: 24,
+    //     address: {
+    //       street: '1326 McKelvey Road',
+    //       city: 'Maryland Heights',
+    //       state: 'MO',
+    //       zip: 63043
+    //     }
+    //   },
+    //   {
+    //     firstName: 'Nigel',
+    //     middleInit: 'C',
+    //     lastName: 'Okello',
+    //     age: 19,
+    //     address: {
+    //       street: '1326 McKelvey Road',
+    //       city: 'Maryland Heights',
+    //       state: 'MO',
+    //       zip: 63043
+    //     }
+    //   },
+    //   {
+    //     firstName: 'Chris',
+    //     middleInit: 'A',
+    //     lastName: 'Okello',
+    //     age: 24,
+    //     address: {
+    //       street: '1326 McKelvey Road',
+    //       city: 'Maryland Heights',
+    //       state: 'MO',
+    //       zip: 63043
+    //     }
+    //   }
+    // ];
+
+    this.showExtended = false;
+
+    // this.addUser({
+    //   firstName: 'Kenneth',
+    //   middleInit: 'R',
+    //   lastName: 'Okello',
+    //   age: 63,
+    //   address: {
+    //     street: '1326 McKelvey Road',
+    //     city: 'Maryland Heights',
+    //     state: 'MO',
+    //     zip: 63043
+    //   }
+    // });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 }
